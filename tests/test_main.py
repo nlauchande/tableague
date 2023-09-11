1f54e6ebab01c38a096335daf7fae022a62ada0e
nlauchande <nlauchande@gmail.com>
2023-09-11 10:17:49 +0300

    Make tests work

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,18 @@
 from typer.testing import CliRunner
+import typer
 
 runner = CliRunner()
 
 from tableague import main as app
 
 def test_app():
-    pass
-    result = runner.invoke(app, ["addscore","--result","Lions 3, Snakes 3"])
-    #assert result.exit_code == 0
-    #assert "Hello Camila" in result.stdout
+    
+    typer.Typer(add_completion=False)
+    result = runner.invoke(app.app, ["--filename","tests/resources/test_main_input.txt"])
+    
+    with open('tests/resources/test_main_output.txt','r') as file:
+        expected_output_content = file.read()
+    
+    assert result.exit_code == 0
+    assert result.stdout == expected_output_content
     
