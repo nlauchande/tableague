cb6329253d17ca4e73d7014e413a225155649e68
nlauchande <nlauchande@gmail.com>
2023-09-11 10:20:22 +0300

    Update format

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,15 @@ runner = CliRunner()
 
 from tableague import main as app
 
-def test_app():
-    
+
+def test_happypath_app():
     typer.Typer(add_completion=False)
-    result = runner.invoke(app.app, ["--filename","tests/resources/test_main_input.txt"])
-    
-    with open('tests/resources/test_main_output.txt','r') as file:
+    result = runner.invoke(
+        app.app, ["--filename", "tests/resources/test_main_input.txt"]
+    )
+
+    with open("tests/resources/test_main_output.txt", "r") as file:
         expected_output_content = file.read()
-    
+
     assert result.exit_code == 0
     assert result.stdout == expected_output_content
-    
